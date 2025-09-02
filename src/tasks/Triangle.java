2a0c970bf0eb60b477a321c7b500bafafb852633
Mikita <redbutterfly4@pm.me>
2025-09-02 10:27:32 +0300

    add triangle task

---
+++ src/tasks/Triangle.java
@@ -0,0 +1,8 @@
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println("   x   ");
+        System.out.println("  xxx  ");
+        System.out.println(" xxxxx ");
+        System.out.println("xxxxxxx");
+    }
+}
